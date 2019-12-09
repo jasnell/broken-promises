85a676b48f0924d9dbabd664dbc082540fb4c8b6
James M Snell <jasnell@gmail.com>
2019-12-08 18:34:13 -0800

    New example

---
+++ Part 1 - That's Not How It Works/RaceConditions/race.js
@@ -0,0 +1,25 @@
+const { writeFile, readFile, unlink } = require('fs').promises;
+const file = './foo.json';
+
+unlink(file).catch(()=>{});  // Ignore
+
+const numbers = [];
+
+async function verify() {
+	const number = Math.random();
+	numbers.push(number);
+	await writeFile(file, JSON.stringify({number}), "utf-8");
+	const data = JSON.parse(await readFile(file,"utf-8"));
+}
+
+(async () => {
+	const promises = [];
+	for (var i = 0; i < 10; i++)
+		promises.push(verify());
+
+	Promise.all(promises)
+		.catch((e) => {
+			console.log(numbers);
+			console.error(e);
+    });
+})();
