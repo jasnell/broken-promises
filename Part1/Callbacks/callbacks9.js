87f906e5ee6b868fa43e76483d74638e7f3216a0
James M Snell <jasnell@gmail.com>
2019-12-09 19:15:07 -0800

    Improvements

--- Part1/Callbacks/callbacks9.js
+++ Part1/Callbacks/callbacks9.js
@@ -2,6 +2,10 @@ const { open, close } = require('fs').promises;
 
 var leakedFd = false;
 
+// By avoiding mixing callbacks and Promises, we keep the
+// code easier to read, easier to reason about, and easier
+// to avoid bugs.
+
 async function openAndClose() {
   let fd;
   try {
